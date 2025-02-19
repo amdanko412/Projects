37e2a22f16124648c37dd9be5dc98515aace2310
amdanko412 <118510360+amdanko412@users.noreply.github.com>
2025-02-19 17:19:14 +0100

    part2b ex2.6

--- part_1/src/App.jsx
+++ part_1/src/App.jsx
@@ -1,47 +1,36 @@
-import { useState } from "react";
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>{text}</button>
-);
+import { useState } from 'react';
 
 const App = () => {
-  const anecdotes = [
-    "If it hurts, do it more often",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
-  ];
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
 
-  const [selected, setSelected] = useState(0);
-
-  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0));
-
-  const addVote = () => {
-    const pointsCopy = [...points];
-    pointsCopy[selected] += 1;
-    setPoints(pointsCopy);
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
   };
 
-  const bestAnecdoteIndex = points.indexOf(Math.max(...points));
+  const addPerson = (event) => {
+    event.preventDefault();
+    setPersons([...persons, { name: newName }]);
+    setNewName('');
+  };
 
   return (
     <div>
-      <h1>Anecdote of the day</h1>
-      <div>{anecdotes[selected]}</div>
-      <div>has {points[selected]} points</div>
-      <Button handleClick={() => addVote()} text="vote" />
-      <Button
-        handleClick={() =>
-          setSelected(Math.floor(Math.random() * anecdotes.length))
-        }
-        text="next anecdote"
-      />
-      <h1>Anecdote with most votes</h1>
-      <div>{anecdotes[bestAnecdoteIndex]}</div>
-      <div>has {points[bestAnecdoteIndex]} votes</div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => (
+          <li key={index}>{person.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
